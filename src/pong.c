479c15e8edd1c19c914cde5f6028a457d1b4b8a1
Arryn Pallas <arrynpal@student.21-school.ru>
2022-02-06 16:26:11 +0000

    Update pong.c

--- src/pong.c
+++ src/pong.c
@@ -1,138 +1,140 @@
 #include <stdio.h>
 
 void check();
-void cross_and_toe_display(); // отрисовка поля
-void step_bord(char play);	// вернет A -> 1 Z -> 2 K -> 3 M -> 4
-int ball_direct = -1, // -1 вверх 0 на месте 1 вниз относительно взгляда
-	x_ball = 18, y_ball = 0;
+void cross_and_toe_display();
+void step_bord(char play);
+int ball_direct = -1, x_ball = 18, y_ball = 0;
 void move_ball();
 int coor_bord_1 = 13, coor_bord_2 = 13;
 int count_1 = 0, count_2 = 0;
-int move_side = 1;	// 1 -> вправо 2 -> влево
+int move_side = 1;
 char play = ' ';
 
 int main() {
-	while (1){
-	move_ball();
-	step_bord(play);
-	printf("\n");
+while (1) {
+move_ball();
+step_bord(play);
+printf("\n");
 
-	for (int i=1; i<=35; i++)
-	printf("%c", ' ');
-	printf("%d\t%d", count_1, count_2);
-	printf("\n");
+for (int i=1; i<=35; i++) printf("%c", ' ');
+printf("%d\t%d", count_1, count_2);
+printf("\n");
 
-	cross_and_toe_display();
+cross_and_toe_display();
 
-	for (int i = 0; i < 80; i++) printf("_"); 
-	if (count_1 == 21){
-		printf("\n");
-		for (int i=1; i <= 18; i++) printf(" "); 
-	printf("Поздравляем с победой игрок 1!");
-	return 0;
-	}
-	if (count_2 == 21){
-	printf("\n");
-	for (int i=1; i <= 18; i++) printf(" ");
-	printf("Поздравляем с победой игрок 2!");
-	return 0;
-	}
-	int time = getchar();
-	if (time != 10) play = (char) time;
-	}
+for (int i = 0; i < 80; i++) printf("_");
+if (count_1 == 21) {
+printf("\n");
+for (int i=1; i <= 18; i++) printf(" ");
+printf("Поздравляем с победой игрок 1!");
+return 0;
+}
+if (count_2 == 21) {
+printf("\n");
+for (int i=1; i <= 18; i++) printf(" ");
+printf("Поздравляем с победой игрок 2!");
+return 0;
+}
+int time = getchar();
+if (time != 10) play = (char) time;
+}
 }
 
-void cross_and_toe_display(){
-	char platforma = '|';
-	char ball = '*';
-	char space = ' ';
-	for (int i = 0; i < 25; i++){ // отрисовка всех строк
-		for (int j = 0; j < 80; j++){ // отрисовка 1 строки
-			if (j == 0){
-				if (i == y_ball && j == x_ball) printf("%c", ball);
-					else if (i == (coor_bord_1 - 1) || i == coor_bord_1 || i == (coor_bord_1 + 1)) printf("%c", platforma);
-				else printf("%c", space);
-			}
-			if (j == 79){
-				if (i == y_ball && j == x_ball) printf("%c", ball);
-					else if (i == (coor_bord_2 - 1) || i == coor_bord_2 || i == (coor_bord_2 + 1)) printf("%c", platforma);
-				else printf("%c", space);
-			}
-			if (j != 0 && j != 79){
-				if (i == y_ball && j == x_ball) printf("%c", ball);
-				else printf("%c", space);
-
-			}
-		}
-	}
+void cross_and_toe_display() {
+char platforma = '|';
+char ball = '*';
+char space = ' ';
+for (int i = 0; i < 25; i++) {
+for (int j = 0; j < 80; j++) {
+if (j == 0) {
+if (i == y_ball && j == x_ball) printf("%c", ball);
+else
+if (i == (coor_bord_1 - 1) || i == coor_bord_1 || i == (coor_bord_1 + 1)) printf("%c", platforma);
+else 
+printf("%c", space);
+}
+if (j == 79) {
+if (i == y_ball && j == x_ball) printf("%c", ball);
+else
+if (i == (coor_bord_2 - 1) || i == coor_bord_2 || i == (coor_bord_2 + 1)) printf("%c", platforma);
+else
+printf("%c", space);
+}
+if (j != 0 && j != 79) {
+if (i == y_ball && j == x_ball) printf("%c", ball);
+else
+printf("%c", space);
+}
+}
+}
 }
 
 void step_bord(char play) {
-	switch (play){ 
-    	case 'A': if  (coor_bord_1 > 1)
-                coor_bord_1 --; break;
-    	case 'Z': if  (coor_bord_1 < 23)
-                coor_bord_1 ++; break;
-    	case 'K': if  (coor_bord_2 > 1)
-                coor_bord_2 --; break;
-    	case 'M': if  (coor_bord_2 < 23)
-                coor_bord_2 ++; break;
-	}
-	play = ' ';
-
+switch (play) {
+case 'A': if  (coor_bord_1 > 1)
+coor_bord_1 --;
+break;
+case 'Z': if  (coor_bord_1 < 23)
+coor_bord_1 ++;
+break;
+case 'K': if  (coor_bord_2 > 1)
+coor_bord_2 --;
+break;
+case 'M': if  (coor_bord_2 < 23)
+coor_bord_2 ++;
+break;
+}
+play = ' ';
 }
 
-void check(){
-	if (x_ball == 78 && // мячик ударяется об правую ракетку
-			(y_ball == (coor_bord_2 - 1) || y_ball == coor_bord_2 || y_ball == (coor_bord_2 + 1))) move_side = 2; // <- влево
-	if (x_ball == 1 && // мячик ударяется об левую ракетку
-			(y_ball == (coor_bord_1 - 1) || y_ball == coor_bord_1 || y_ball == (coor_bord_1 + 1))) move_side = 1; // -> вправо
-	if (y_ball == 0 || (y_ball == (coor_bord_1 + 1) && x_ball == 1) || (y_ball == (coor_bord_2 + 1) && x_ball == 78)) ball_direct = 1;
-	if (y_ball == 24 || (y_ball == (coor_bord_1 - 1) && x_ball == 1) || (y_ball == (coor_bord_2 - 1) && x_ball == 78)) ball_direct = -1;
-	if ((y_ball == coor_bord_1 && x_ball == 1) || (y_ball == coor_bord_2 && x_ball == 78)) ball_direct = 0;
-	if (x_ball == 79){
-		count_1 ++;
-		move_side = 1;
-		x_ball = 13;
-		y_ball = 13;
-		ball_direct = -1;
-	}
-	if (x_ball == 0){
-		count_2 ++;
-		move_side = 2;
-		x_ball = 76;
-		y_ball = 13;
-		ball_direct = -1;
-	}
+void check() {
+if (x_ball == 78 && (y_ball == (coor_bord_2 - 1) || y_ball == coor_bord_2 || y_ball == (coor_bord_2 + 1))) move_side = 2;
+if (x_ball == 1 && (y_ball == (coor_bord_1 - 1) || y_ball == coor_bord_1 || y_ball == (coor_bord_1 + 1))) move_side = 1;
+if (y_ball == 0 || (y_ball == (coor_bord_1 + 1) && x_ball == 1) || (y_ball == (coor_bord_2 + 1) && x_ball == 78)) ball_direct = 1;
+if (y_ball == 24 || (y_ball == (coor_bord_1 - 1) && x_ball == 1) || (y_ball == (coor_bord_2 - 1) && x_ball == 78)) ball_direct = -1;
+if ((y_ball == coor_bord_1 && x_ball == 1) || (y_ball == coor_bord_2 && x_ball == 78)) ball_direct = 0;
+if (x_ball == 79) {
+count_1 ++;
+move_side = 1;
+x_ball = 13;
+y_ball = 13;
+ball_direct = -1;
+}
+if (x_ball == 0) {
+count_2 ++;
+move_side = 2;
+x_ball = 76;
+y_ball = 13;
+ball_direct = -1;
+}
 }
 
-void move_ball(){
-	check();
+void move_ball() {
+check();
+if (move_side == 1) {
+if (ball_direct == -1 && y_ball != 0) {
+y_ball --;
+x_ball ++;
+}
+else
+if (ball_direct == 1 && y_ball != 24) {
+y_ball ++;
+x_ball ++;
+}
+else
+x_ball ++;
+}
 
-	// вправо ->
-	if (move_side == 1){
-		if (ball_direct == -1 && y_ball != 0){
-			y_ball --;
-			x_ball ++;
-		}
-		// if (ball_direct == 0) x_ball ++;
-		else if (ball_direct == 1 && y_ball != 24){
-			y_ball ++;
-			x_ball ++;
-		}
-		else x_ball ++;
-	}
-	// влево <-
-	if (move_side == 2){
-		if (ball_direct == -1 && y_ball != 0){
-			y_ball --;
-			x_ball --;
-		}
-		else if (ball_direct == 1 && y_ball != 24){
-			y_ball ++;
-			x_ball --;
-		}
-		else x_ball --;
-	}
-
+if (move_side == 2) {
+if (ball_direct == -1 && y_ball != 0) {
+y_ball --;
+x_ball --;
+}
+else
+if (ball_direct == 1 && y_ball != 24) {
+y_ball ++;
+x_ball --;
+}
+else x_ball --;
+}
 }
